82df46c5f282a7e8457a2a9919c3071650dfe29d
Fatemeh Dastgheib <fatemeh.dastgheib@gmail.com>
2025-06-02 14:18:34 +0200

    Update and convert to react app build with vite

---
+++ Portfolio-2025/src/App.tsx
@@ -0,0 +1,14 @@
+import MainPage from "./MainPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
